fe7529fd5a180755727e059432e786af6fc5efb7
Lachlan Mackenzie <19154190@student.curtin.edu.au>
2020-04-06 22:00:10 +0800

    Remove prints

--- tvmazepy/tvmaze.py
+++ tvmazepy/tvmaze.py
@@ -11,9 +11,8 @@ from .model.person import Character, Person, Crew
 
 
 class TVmaze:
-    def __init__(self, log=False):
-        self.log = log
-        print('TVmaze object created.')
+    def __init__(self):
+        pass
 
     def _query_api(self, url, params=None):
         res = requests.get(url, params)
@@ -24,12 +23,10 @@ class TVmaze:
         return res.json()
 
     def search_show(self, show_name):
-        print(f'Searching shows with name: {show_name}')
         res = self.query_api(endpoints.search_show_name, {'q': show_name})
         return [Show(show) for show in res] if res is not None else []
 
     def search_show_best_match(self, show_name, embed=None):
-        print(f'Searching a show with name: {show_name}')
         res = self.query_api(endpoints.search_show_best_match, {'q': show_name, 'embed': embed})
         return Show(res) if res is not None else None
 
@@ -43,28 +40,23 @@ class TVmaze:
         return self._get_show_external_id('tvrage', tvrage_id)
 
     def _get_show_external_id(self, external_name, external_id):
-        print(f'Searching show with {external_name} ID: {external_id}')
         res = self.query_api(endpoints.search_external_show_id, {external_name: external_id})
         return Show(res) if res is not None else None
 
     def get_show(self, tvmaze_id, embed=None):
-        print(f'Searching show with TVmaze ID: {tvmaze_id}')
         res = self.query_api(endpoints.show_information.format(str(tvmaze_id)), {'embed': embed})
         return Show(res) if res is not None else None
 
     def get_show_episode_list(self, tvmaze_id, specials=False):
-        print(f'Episodes of show with TVmaze ID: {tvmaze_id}')
         specials = 1 if specials else None
         res = self.query_api(endpoints.show_episode_list.format(str(tvmaze_id)), {'specials': specials})
         return [Episode(episode) for episode in res] if res is not None else []
 
     def get_show_episode(self, tvmaze_id, season, episode):
-        print(f'Episode S{season}E{episode} of show with TVmaze ID: {tvmaze_id}')
         res = self.query_api(endpoints.show_episode.format(str(tvmaze_id)), {'season': season, 'number': episode})
         return Episode(res) if res is not None else None
 
     def get_show_episodes_by_date(self, tvmaze_id, date_input):
-        print(f'Episode released on {date_input} of show with TVmaze ID: {tvmaze_id}')
         if type(date_input) is str:
             try:
                 date = parser.parse(date_input).isoformat()[:10]
@@ -78,32 +70,26 @@ class TVmaze:
         return [Episode(episode) for episode in res] if res is not None else []
 
     def get_show_season_list(self, tvmaze_id):
-        print(f'Seasons of show with TVmaze ID: {tvmaze_id}')
         res = self.query_api(endpoints.show_season_list.format(str(tvmaze_id)))
         return [Season(season) for season in res] if res is not None else []
 
     def get_season_episode_list(self, tvmaze_season_id):
-        print(f'Episodes of season with TVmaze season ID: {tvmaze_season_id}')
         res = self._query_api(endpoints.season_episode_list.format(str(tvmaze_season_id)))
         # episode['number'] is None when it is classed as a special, for now don't include specials
         return [Episode(episode) for episode in res if episode['number'] is not None] if res is not None else []
 
     def get_show_aliases(self, tvmaze_id):
-        print(f'Aliases of show with TVmaze season ID: {tvmaze_id}')
         res = self._query_api(endpoints.show_alias_list.format(str(tvmaze_id)))
         return [Alias(alias) for alias in res] if res is not None else []
 
     def get_episode_information(self, tvmaze_episode_id, embed=None):
-        print(f'Episode of TVmaze episode ID: {tvmaze_episode_id}')
         res = self._query_api(endpoints.episode_information.format(str(tvmaze_episode_id)), {'embed': embed})
         return Episode(res) if res is not None else None
 
     def get_show_cast(self, tvmaze_id):
-        print(f'Cast of show with TVmaze ID: {tvmaze_id}')
         res = self._query_api(endpoints.show_cast.format(str(tvmaze_id)))
         return [Character(cast['character'], cast['person']) for cast in res]
 
     def get_show_crew(self, tvmaze_id):
-        print(f'Cast of show with TVmaze ID: {tvmaze_id}')
         res = self._query_api(endpoints.show_crew.format(str(tvmaze_id)))
         return [Crew(crew_member) for crew_member in res]
